230062722467e9972fed82819c657bcd515b595a
mesequeteixeira <57111262+manoelmeseque@users.noreply.github.com>
2021-11-29 21:41:51 -0300

    Adding Style to Repositories

--- 01-github-explorer/src/App.jsx
+++ 01-github-explorer/src/App.jsx
@@ -1,12 +1,10 @@
 import '../src/styles/global.scss'
-import { Counter } from './components/Counter'
 import { RepositorList } from './components/RepositoryList'
 
 export function App() {
     return (
         <>
             <RepositorList />
-            <Counter />
         </>
     )
 }

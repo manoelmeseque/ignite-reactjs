e21effbb2028097ce1b4bdb266854c7205208654
mesequeteixeira <57111262+manoelmeseque@users.noreply.github.com>
2021-11-30 13:22:04 -0300

    converting fields to typescript

--- 01-github-explorer/src/components/RepositoryList.tsx
+++ 01-github-explorer/src/components/RepositoryList.tsx
@@ -3,17 +3,15 @@ import { RepositoryItem } from "./RepositoryItem";
 
 import '../styles/repositories.scss'
 
-const repository = {
-    name: 'Unform',
-    description: 'Unform in React',
-    link: 'https://github.com/unform/unform'
+interface Repository{
+    name: string,
+    description: string,
+    html_url: string,
 }
-
-// https://api.github.com/orgs/rocketseat/repos
 
 export function RepositorList() {
 
-    const [repositories, setRepositories] = useState([])
+    const [repositories, setRepositories] = useState<Repository[]>([])
 
     useEffect(() => {
         fetch('https://api.github.com/orgs/rocketseat/repos')

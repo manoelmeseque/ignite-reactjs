4950e678504b4b95b89e08178415bf1b9c084ab8
mesequeteixeira <57111262+manoelmeseque@users.noreply.github.com>
2021-12-03 11:40:47 -0300

    styling the modal

--- dtmoney/src/components/NewTransactionModal/index.tsx
+++ dtmoney/src/components/NewTransactionModal/index.tsx
@@ -1,4 +1,6 @@
 import Modal from 'react-modal'
+import { Container } from './styles'
+import closeImg from '../../assets/close.svg'
 
 interface NewTransactionModalProps {
     isOpen: boolean;
@@ -10,8 +12,24 @@ export function NewTransactionModal({ isOpen, onRequestClose}: NewTransactionMod
         <Modal
             isOpen={isOpen}
             onRequestClose={onRequestClose}
+            overlayClassName="react-modal-overlay"
+            className="react-modal-content"
         >
-          <h2>Cadastro de transação</h2>
+            <button
+                type="button"
+                onClick={onRequestClose}
+                className="react-modal-close"
+            >
+                <img src={closeImg} alt="fecha modal" />
+            </button>
+            <Container>
+                <h2>Cadastro de transação</h2>
+
+                <input type="text" placeholder="Título" />
+                <input type="number" placeholder="Valor" />
+                <input type="text" placeholder="Categoria" />
+                <button type="submit">Cadastrar</button>
+            </Container>
         </Modal>
     )
 }

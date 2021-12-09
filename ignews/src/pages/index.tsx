86ce579bbe5aac85474c26928d166b071ce47c61
mesequeteixeira <57111262+manoelmeseque@users.noreply.github.com>
2021-12-09 10:53:46 -0300

    adding new components Header and SingInButton

--- ignews/src/pages/index.tsx
+++ ignews/src/pages/index.tsx
@@ -6,7 +6,17 @@ export default function Home() {
       <Head>
         <title>Home | ig.news</title>
       </Head>
-      <div>Hello world</div>
+      <main>
+        <section>
+          <span>👏 Hey, welcome</span>
+          <h1>News about the <span>React</span> world</h1>
+          <p>
+            Get access to all publications <br />
+            <span>for $9.90 month</span>
+          </p>
+        </section>
+        <img src="/images/avatar.svg" alt="girl coding" />
+      </main>
     </>
   )
 }

476007d7828de90826611fd86754a10a6d74b501
mesequeteixeira <57111262+manoelmeseque@users.noreply.github.com>
2021-12-10 11:43:24 -0300

    connect stripe

--- ignews/src/pages/index.tsx
+++ ignews/src/pages/index.tsx
@@ -1,9 +1,18 @@
+import { GetStaticProps } from 'next'
 import Head from 'next/head'
+import { stripe } from '../services/stripe'
 import { SubscribeButton } from '../components/SubscribeButton'
 
 import styles from './home.module.scss'
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceId: string,
+    amount: number
+  }
+}
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
@@ -15,12 +24,57 @@ export default function Home() {
           <h1>News about the <span>React</span> world</h1>
           <p>
             Get access to all publications <br />
-            <span>for $9.90 month</span>
+            <span>for {product.amount} month</span>
           </p>
-          <SubscribeButton />
+          <SubscribeButton priceId={product.priceId} />
         </section>
         <img src="/images/avatar.svg" alt="girl coding" />
       </main>
     </>
   )
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+
+  const price = await stripe.prices.retrieve('price_1K4nn1EWccdJEhOrqmzfNL5i', {
+    expand: ['product']
+  })
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US',{
+      style: 'currency',
+      currency: 'USD'
+    }).format(price.unit_amount / 100)
+  }
+
+  return {
+    props: {
+      product
+    },
+    revalidate: 60 * 60 * 24 //24 horas depois disso a página vai ser reconstruida
+  }
+}
+
+// Forma de fazer com SSR 
+
+// export const getServerSideProps: GetServerSideProps = async () => {
+
+//   const price = await stripe.prices.retrieve('price_1K4nn1EWccdJEhOrqmzfNL5i', {
+//     expand: ['product']
+//   })
+
+//   const product = {
+//     priceId: price.id,
+//     amount: new Intl.NumberFormat('en-US',{
+//       style: 'currency',
+//       currency: 'USD'
+//     }).format(price.unit_amount / 100)
+//   }
+
+//   return {
+//     props: {
+//       product
+//     }
+//   }
+// }

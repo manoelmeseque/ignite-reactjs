253eccfc99a1f89dd8830bfd842093e73aacbe11
mesequeteixeira <57111262+manoelmeseque@users.noreply.github.com>
2021-12-10 20:49:42 -0300

    adding connection faunadb

--- ignews/src/components/SingInButton/index.tsx
+++ ignews/src/components/SingInButton/index.tsx
@@ -7,8 +7,6 @@ import styles from './styles.module.scss'
 export function SingInButton() {
     const {data: session} = useSession()
 
-    console.log(session)
-
     return session ? (
         <button
             type="button"

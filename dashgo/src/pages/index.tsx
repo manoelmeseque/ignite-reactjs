40e6a18a7b8bb058ca47d667a38469e97c65d98a
mesequeteixeira <57111262+manoelmeseque@users.noreply.github.com>
2021-12-17 14:25:37 -0300

    adding news components

--- dashgo/src/pages/index.tsx
+++ dashgo/src/pages/index.tsx
@@ -1,8 +1,38 @@
-import Head from 'next/head'
-import Image from 'next/image'
+import { Flex, Button, Stack } from '@chakra-ui/react'
+import { Input } from '../components/Form/Input'
 
-export default function Home() {
+export default function SignIn() {
   return (
-    <h1>Dashgo</h1>
+    <Flex w="100vw" h="100vh" align="center" justify="center">
+      <Flex
+        as="form"
+        width="100%"
+        maxWidth="365px"
+        bg="gray.800"
+        p="8"
+        borderRadius="8"
+        flexDir="column"
+      >
+        <Stack spacing="4">
+
+          <Input
+            name="email"
+            label="E-mail"
+            type="email"
+          />
+
+          <Input
+            name="password"
+            label="Senha"
+            type="password"
+          /> 
+          
+        </Stack>
+        
+        <Button type="submit" mt="6" colorScheme="pink" size="lg">
+          Entrar
+        </Button>
+      </Flex>
+    </Flex>
   )
 }

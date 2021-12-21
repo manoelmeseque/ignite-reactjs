9f6b10b9b01d282c0188bda921223378b4bddffb
mesequeteixeira <57111262+manoelmeseque@users.noreply.github.com>
2021-12-21 11:31:43 -0300

    adding create user

--- dashgo/src/pages/users/create.tsx
+++ dashgo/src/pages/users/create.tsx
@@ -1,7 +1,6 @@
-import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Th, Thead, Tr, Text } from "@chakra-ui/react";
-import { RiAddLine, RiPencilLine } from "react-icons/ri";
+import { Box, Button, Divider, Flex, Heading, HStack, SimpleGrid, VStack } from "@chakra-ui/react";
+import { Input } from "../../components/Form/Input";
 import Header from '../../components/Header'
-import Pagination from "../../components/Pagination";
 import SideBar from '../../components/SideBar'
 
 export default function CreateUser() {
@@ -13,7 +12,30 @@ export default function CreateUser() {
                 <SideBar /> 
                 
                 <Box flex="1" borderRadius={8} bg="gray.800" p="8">
-                    
+                    <Heading size="lg" fontWeight="normal">Usuários</Heading>
+
+                    <Divider my="6" borderColor="gray.700" />
+
+                    <VStack spacing="8">
+                        <SimpleGrid minChildWidth="240px" spacing="8" width="100%">
+                            <Input name="name" label="Name Completo" />
+                            <Input name="email" type="email" label="E-mail" />
+                        </SimpleGrid>
+
+                        <SimpleGrid minChildWidth="240px" spacing="8" width="100%">
+                            <Input name="password" type="password" label="Senha" />
+                            <Input name="confirm_password" type="password" label="Confirmação de senha" />
+                        </SimpleGrid>
+                    </VStack>
+
+                    <Flex mt="8" justify="flex-end">
+
+                        <HStack spacing="4">
+                            <Button colorScheme="whiteAlpha">Cancelar</Button>
+                            <Button colorScheme="pink">Salvar</Button>
+                        </HStack>
+
+                    </Flex>
                 </Box>
             </Flex>
 
